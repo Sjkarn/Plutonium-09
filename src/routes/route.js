a9d2ae63f274e8d6a29ffd28d9648adcf7569aaf
Saurabh Karn <saurabhkarn007@gmail.com>
2022-08-08 21:47:00 +0530

    break-time assignment

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const lodash = require('lodash');
 const abc = require('../introduction/intro')
 const logg = require('../logger/loggers')
 const help = require('../util/helper')
@@ -20,6 +21,26 @@ router.get('/test-me', function (req, res) {
     console.log(valid.upper)
     // logg.printName()
     // abc.printName()
+
+    const arr = ["January","Febuary","March","April","May","June","July","August","September","Octuber","November","December"]
+    let result1 = lodash.chunk(arr,[size=4]);
+    console.log(result1)
+
+    const getOdd = [3,5,7,9,11,13,15,17,19,21]
+    let findOdd = lodash.tail(getOdd);
+    console.log(findOdd)
+
+    const arr1 = [1,4,7,8]
+    const arr2 = [5,3,4,6]
+    const arr3 = [1,2,9,5]
+    const arr4 = [7,1,12,31]
+    let newArr = lodash.union(arr1,arr2,arr3,arr4);
+    console.log(newArr)
+
+    const array = [['horror','The Shining'],['drama','Titanic'],['thriller','Shutter Island'],['fantasy','Pans Labyrinth']];
+    let arrToObj = lodash.fromPairs(array);
+    console.log(arrToObj)
+
     res.send('My second ever api!')
 });
 
