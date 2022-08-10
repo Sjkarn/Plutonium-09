9bf32d403c5946fca59dd1d19e8127f966d8fa29
Saurabh Karn <saurabhkarn007@gmail.com>
2022-08-10 13:02:36 +0530

    done

--- src/routes/route.js
+++ src/routes/route.js
@@ -166,7 +166,42 @@ router.get('/test-me', function (req, res) {
        
         res.send(  { data: missingNumber  }  );
       });
-     
+      
+      router.post('/players', function (req, res) {
+        let players =
+   [
+       {
+           "name": "manish",
+           "dob": "1/1/1995",
+           "gender": "male",
+           "city": "jalandhar",
+           "sports": [
+               "swimming"
+           ]
+       },
+       {
+           "name": "gopal",
+           "dob": "1/09/1995",
+           "gender": "male",
+           "city": "delhi",
+           "sports": [
+               "soccer"
+           ],
+       },
+       {
+           "name": "lokesh",
+           "dob": "1/1/1990",
+           "gender": "male",
+           "city": "mumbai",
+           "sports": [
+               "soccer"
+           ],
+       },
+   ]
+
+        res.send(  { data: players , status: true }  )
+    });
+ 
     res.send('My second ever api!')
 });
 
