fc71f2634fe30994be9ab923067f624825446b10
Saurabh Karn <saurabhkarn007@gmail.com>
2022-09-22 11:41:19 +0530

    Assignment Done

--- src/controllers/cowinController.js
+++ src/controllers/cowinController.js
@@ -43,7 +43,7 @@ let getByPin = async function (req, res) {
         let pin = req.query.pincode
         let date = req.query.date
         console.log(`query params are: ${pin} ${date}`)
-        var options = {
+        let options = {
             method: "get",
             url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByPin?pincode=${pin}&date=${date}`
         }
@@ -62,7 +62,7 @@ let getOtp = async function (req, res) {
         let blahhh = req.body
         
         console.log(`body is : ${blahhh} `)
-        var options = {
+        let options = {
             method: "post",
             url: `https://cdn-api.co-vin.in/api/v2/auth/public/generateOTP`,
             data: blahhh
@@ -78,8 +78,71 @@ let getOtp = async function (req, res) {
     }
 }
 
+let getByDistrictId = async function (req, res) {
+    try {
+        let districtId = req.query.district_id
+        let date = req.query.date
+        console.log(`query params are: ${districtId} ${date}`)
+        let options = {
+            method: "get",
+            url: `https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/findByDistrict?district_id=${districtId}&date=${date}`
+        }
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
 
+let getSortedCities = async function (req, res) {
+    try {
+        let cities = ["Bengaluru","Mumbai","Delhi","Kolkata","Chennai","London","Moscow"]
+        let cityObjArray = []
+        for (i=0; i<cities.length; i++) {
+            let obj = { city: cities[i]}
+            let resp = await axios.get (`http://api.openweathermap.org/data/2.5/weather?q=${cities[i]}&appid=fe63605731706de3634179916fe10695`)
+            console.log(resp.data.main.temp)
+            obj.temp = resp.data.main.temp
+            cityObjArray.push(obj)
+        }
+        let sorted = cityObjArray.sort(function (a, b) {return a.temp - b.temp})
+        console.log(sorted)
+        res.status(200).send({status: true, data: sorted})
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message })
+    }
+}
+
+let memeHandler = async function (req, res){
+    try {
+        let memeId = req.query.template_id
+        let text0 = req.query.text0
+        let text1 = req.query.text1
+        let name = req.query.username
+        let password = req.query.password
+        console.log(`query params are: ${template_id} ${text0} ${text1} ${username} ${password}`)
+        let options = {
+            method: "post",
+            url: `https://imgflip.com/i/6s08ub?template_id=${memeId}&text0=${text0}&text1=${text1}&username=${name}&password=${password}`
+        }
+        let result = await axios(options)
+        console.log(result.data)
+        res.status(200).send({ msg: result.data })
+    }
+    catch (err) {
+        console.log(err)
+        res.status(500).send({ msg: err.message }) 
+    }
+}
 module.exports.getStates = getStates
 module.exports.getDistricts = getDistricts
 module.exports.getByPin = getByPin
 module.exports.getOtp = getOtp
+module.exports.getByDistrictId = getByDistrictId
+module.exports.getSortedCities = getSortedCities
+module.exports.memeHandler = memeHandler
